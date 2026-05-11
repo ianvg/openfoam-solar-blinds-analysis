a327fbeba8a2cc3188b19ec849ec844377e207cb
Ian Van Giesen <ianvangiesen@gmail.com>
2026-05-11 16:43:12 +0200

    Update with lots of comments and provide code for optional run in parallel for the simulation.

--- run_solver.sh
+++ run_solver.sh
@@ -2,10 +2,17 @@
 #To run use "bash run_solver.sh"
 set -euo pipefail #So we know if the first command out of two piped commands fails
 
+#######################################################
+### Cleaning the case
+#######################################################
 
 foamCleanTutorials
 rm -r 0 > /dev/null 2>&1
 rm -rf constant/polyMesh processor*
+
+#######################################################
+### Creating and checking the mesh
+#######################################################
 
 blockMesh -dict system/blockMeshDict | tee log.blockMesh
 surfaceFeatures | tee log.surfaceFeatures
@@ -22,12 +29,29 @@ checkMesh -constant | tee log.checkMesh
 
 checkMesh -constant -allTopology | grep "cell zones"
 
-
 #Copy over everything from 0_org/ folder to 0/
 rm -rf 0
 cp -r 0_org 0
 
-#Running the solver below:
+#######################################################
+### Running the simulation
+#######################################################
+
+#Ensure that either the solver in serial or parallel is uncommented.
+
+#Running the solver in serial:
 foamRun -solver incompressibleFluid | tee log.solver
+
+#Running the solver in parallel:
+
+#Decomposing the mesh to prepare it for running in parallel
+#Ensure that you have the minimum number of processors available for this job before running it!
+#decomposePar -force
+#mpirun -np 4 -solver incompressibleFluid -parallel | tee log.solver
+#reconstructPar #To reconstruct the parallel case: 
+
+#######################################################
+### Post-processing
+#######################################################
 
 foamPostProcess -solver incompressibleFluid -func yPlus -latestTime -noFunctionObjects | tee log.yPlus
