a3ea0831ceca4453c42bf27d2025480f3471a2f9
Ian Van Giesen <ianvangiesen@gmail.com>
2026-05-19 17:55:39 +0200

    working case 4 with Cd discharge calculated

--- C4/run_postprocess.sh
+++ C4/run_postprocess.sh
@@ -10,3 +10,13 @@ foamPostProcess -func sampleDict -latestTime
 
 # inletOutletPressureDrop post-processing
 foamPostProcess -func sampleDict1 -latestTime
+
+# Using python tool to get average of each line
+python3 tools/average_pGauge_lines.py
+
+# You can also choose specific times (if they already be sampleDict'd on:
+# python3 tools/average_pGauge_lines.py --time 60
+# Or choose to target pPa instead:
+# python3 tools/average_pGauge_lines.py --column pPa
+
+
